03138c6c33f9c70b11192259716552f8bcd2ffe0
GreenHillary <79810984+GreenHillary@users.noreply.github.com>
2021-12-07 20:40:36 -0600

    Adding a button with prompt to enter a choice

--- scripts.js
+++ scripts.js
@@ -2,3 +2,14 @@ function computerPlay {
     let choice = "scissors"
     return choice;
 }
+
+function myFunction() {
+    let text;
+    let person = prompt("Rock, paper, or scissors?", "First Last");
+    if (person == null || person == "") {
+      text = "User cancelled the prompt.";
+    } else {
+      text = "Winner is:" + person + "Congratulations!";
+    }
+    document.getElementById("demo").innerHTML = text;
+  }
